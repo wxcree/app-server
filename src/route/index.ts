62afeb1dd41aae701389a674fc750abc7141d177
wxc <wangxiaocheng112@icloud.com>
2022-04-21 21:26:01 +0800

    update: 更新

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,13 +1,11 @@
 import test from './routes/test';
-import extra from './routes/extra';
 import auth from './routes/auth';
 import table from './routes/table';
 import pkgs from './routes/pkgs'
 
 export default { 
-    extra, 
-    test,
     auth,
     table,
     pkgs,
+    test,
 };
